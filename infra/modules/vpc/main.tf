d56e731ae5d420777066e5880b187754ee44da1a
Chuck <cjoseph246@gmail.com>
2025-04-14 18:19:41 -0400

    vpc - move harcoded values to tg file

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -6,29 +6,20 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.19"
 
-  name = "eks-vpc"
-  cidr = "10.0.0.0/16"
+  name = var.vpc_name
+  cidr = var.vpc_cidr
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = var.availability_zones
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway = var.enable_nat_gateway
+  single_nat_gateway = var.single_nat_gateway
 
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/elb" = "1"
-  }
+  public_subnet_tags  = var.public_subnet_tags
+  private_subnet_tags = var.private_subnet_tags
 
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb" = "1"
-  }
-
-  tags = {
-    "kubernetes.io/cluster/eks-cluster" = "shared"
-  }
+  tags = var.tags
 }
 
 terraform {
